18603ae555440d29c51eb807f76d4357a5d5100b
Abdalla Omer <abdo_hu_90@hotmail.com>
2025-08-23 15:24:55 +0300

    finalizing the home screen ui with the need for images fixes

--- app/src/main/java/com/example/comida/screens/HomeScreen.kt
+++ app/src/main/java/com/example/comida/screens/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -23,6 +25,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -527,7 +530,163 @@ private fun AvailableRestaurants(
             horizontalArrangement = Arrangement.spacedBy(20.dp)
         ){
             items(restaurants) { restaurant ->
+                Card(
+                    modifier = Modifier
+                        .wrapContentSize(),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    ),
+                    shape = RoundedCornerShape(24.dp),
+                    elevation = CardDefaults.cardElevation(4.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ){
+                            Image(
+                                painter = painterResource(restaurant.image),
+                                contentDescription = restaurant.name,
+                                contentScale = ContentScale.FillBounds,
+                                modifier = Modifier
+                                    .height(120.dp)
+                                    .width(300.dp)
+                            )
 
+                            Button(
+                                modifier = Modifier
+                                    .align(Alignment.TopEnd),
+                                onClick = {
+                                    onToggleIsFavoriteTapped(restaurant.isFavorites)
+                                },
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color.Transparent
+                                )
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.heart_icon),
+                                    contentDescription = "Toggle Favorites Icon Button",
+                                    tint = Color.White,
+                                    modifier = Modifier
+                                        .clip(CircleShape)
+                                        .background(Color.White.copy(alpha = 0.3f))
+                                        .padding(8.dp)
+                                        .size(24.dp)
+                                )
+                            }
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(start = 16.dp, top = 8.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+
+                        ) {
+                            Text(
+                                text = restaurant.name,
+                                fontFamily = poppinsFamily,
+                                fontSize = 15.sp,
+                                fontWeight = FontWeight.SemiBold,
+                                color = PrimaryTextColor,
+                            )
+
+                            Spacer(
+                                modifier = Modifier
+                                    .width(80.dp)
+                            )
+
+                            Row(
+                                modifier = Modifier,
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                Icon(
+                                    painter = painterResource(R.drawable.star),
+                                    contentDescription = "Rating Icon",
+                                    tint = OnboardingBackgroundColor3,
+                                    modifier = Modifier
+                                        .size(16.dp)
+                                )
+
+                                Text(
+                                    text = "${restaurant.ratingValue}",
+                                    fontFamily = bebasFamily,
+                                    fontSize = 10.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = PrimaryTextColor
+                                )
+                            }
+                        }
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .padding(start = 16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+
+                                Icon(
+                                    painter = painterResource(R.drawable.delivery_icon),
+                                    contentDescription = "Delivery Icon",
+                                    tint = PrimaryButtonColor,
+                                    modifier = Modifier
+                                        .size(16.dp)
+                                )
+
+                                Text(
+                                    text = if (restaurant.isFreeDelivery) "Free delivery" else "$5 Delivery fee",
+                                    fontWeight = FontWeight.Normal,
+                                    fontFamily = bebasFamily,
+                                    fontSize = 12.sp,
+                                    color = SmallIconLabelColor
+                                )
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .width(150.dp)
+                            )
+
+                            Row(
+                                modifier = Modifier,
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+
+                                Icon(
+                                    painter = painterResource(R.drawable.timer_icon),
+                                    contentDescription = "Delivery Icon",
+                                    tint = PrimaryButtonColor,
+                                    modifier = Modifier
+                                        .size(16.dp)
+                                )
+
+                                Text(
+                                    text = "${restaurant.deliveryTime}",
+                                    fontWeight = FontWeight.Normal,
+                                    fontFamily = bebasFamily,
+                                    fontSize = 12.sp,
+                                    color = SmallIconLabelColor
+                                )
+                            }
+                        }
+
+                        Spacer(
+                            modifier = Modifier
+                                .height(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
